86214543560bab27148dd098caf0253d12dcd808
MerveALKAN <17701037@mersin.edu.tr>
2021-05-21 22:34:30 +0300

    21.05.2021

---
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobPositionService.java
@@ -0,0 +1,12 @@
+package kodlamaio.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.hrms.entities.concretes.JobPosition;
+
+public interface JobPositionService {
+
+	List<JobPosition> getAll();
+	
+	
+}
